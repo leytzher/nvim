9e45372bdcd516abc3b606ccc287fad88bc1a91c
stim.ai <leytzher@gmail.com>
2022-04-01 08:07:14 +0400

    added status bar

--- init.lua
+++ init.lua
@@ -6,5 +6,4 @@ require('core.plugins-config.nvim-treesitter')
 require('core.plugins-config.telescope')
 require('core.plugins-config.toggleterm')
 require('core.plugins-config.whichkey')
-require('core.plugins-config.lualine')
 
--- lua/packer_init.lua
+++ lua/packer_init.lua
@@ -126,9 +126,38 @@ return packer.startup(function(use)
         end,
     }  -- onedark color theme
 
-    use {
-      'nvim-lualine/lualine.nvim',
-      requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-}
+     use {'datwaft/bubbly.nvim', config = function()
+        -- Here you can add the configuration for the plugin
+        vim.g.bubbly_palette = {
+          background = "#34343c",
+          foreground = "#c5cdd9",
+          black = "#3e4249",
+          red = "#ec7279",
+          green = "#a0c980",
+          yellow = "#deb974",
+          blue = "#6cb6eb",
+          purple = "#d38aea",
+          cyan = "#5dbbc1",
+          white = "#c5cdd9",
+          lightgrey = "#57595e",
+          darkgrey = "#404247",
+        }
+        vim.g.bubbly_statusline = {
+          'mode',
+
+          'truncate',
+
+          'path',
+          'branch',
+          'signify',
+          'gitsigns',
+          'coc',
+
+          'divisor',
+
+          'filetype',
+          'progress',
+        }
+          end}
 end)
 
