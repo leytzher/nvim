a216f3d9e4bdf43c7193eba10c183645d7c47d31
stim.ai <leytzher@gmail.com>
2022-04-01 08:25:01 +0400

    added completions

--- lua/packer_init.lua
+++ lua/packer_init.lua
@@ -159,5 +159,20 @@ return packer.startup(function(use)
           'progress',
         }
           end}
+
+    use "hrsh7th/nvim-cmp" -- The completion plugin
+    use "hrsh7th/cmp-buffer" -- buffer completions
+    use "hrsh7th/cmp-path" -- path completions
+    use "hrsh7th/cmp-cmdline" -- cmdline completions
+    use "saadparwaiz1/cmp_luasnip" -- snippet completions
+
+    -- snippets
+    use "L3MON4D3/LuaSnip" --snippet engine
+    use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+
+    if PACKER_BOOTSTRAP then
+        require("packer").sync()
+    end
 end)
+
 
